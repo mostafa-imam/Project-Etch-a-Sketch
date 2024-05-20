35996c2efcce669fa3d1827e5d4ca8fd5a8eeb3e
Mostafa Imam <137308525+mostafa-imam@users.noreply.github.com>
2024-05-20 05:49:25 +0300

    feat: Create a loop that creates grid of divs

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 const container = document.querySelector('.container');
 const btn = document.querySelector('button');
+
+let initialInput = 16;
+
+for (let i = 0; i < initialInput; i++) {
+
+    let rowContainer = document.createElement('div');
+    rowContainer.classList.add('row-container');
+
+    for (let x = 0; x < initialInput; x++) {
+
+        let squareDiv = document.createElement('div');
+        squareDiv.classList.add('square-div');
+        rowContainer.appendChild(squareDiv);
+
+    }
+
+    container.appendChild(rowContainer);
+
+};
